ebdc1d85c05517341aaa6372c54aa58204e673b5
jordyismael08 <50557504+jordyismael08@users.noreply.github.com>
2022-08-04 00:01:29 -0500

    primer commit

---
+++ inc/footer.php
@@ -0,0 +1,39 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container"> 
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                        <i class="fa fa-phone" aria-hidden="true">&nbsp; Telefono: 0998079238 </i> 
+                    </a><br>
+                
+                <a href="https://goo.gl/maps/ZQ7CGnAgkyLrEdgCA" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                    <h1>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.236788571621!2d-80.69474408590031!3d-0.9782572356420444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902be57c5bf5359b%3A0xc6ee1fab126ed2c0!2sBazar%20y%20Papeler%C3%ADa%20Jesymel!5e0!3m2!1ses!2sec!4v1659006165124!5m2!1ses!2sec" 
+                        width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </h1>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Sobre Nosotros</h4>
+                 
+                
+                <p> 
+                Bazar y Papeleria Jesymel somos una empresa de servicio</br>
+                que ofrece una gran variedad de artículos.Desde 2005</br>
+                desempeñamos nuestra labor para satisfacer al cliente y</br>
+                que confíen en nuestra amplia gama de productos de primeras marcas.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Dirección</h4>
+                <p style="color: #FFF">28C4+MXW, Calle 64, Manta</p>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">B&P Jesymel &copy; 2022</h5>
+</footer>
